1e1dc56df098fb00d40b92cb9884c389172b346a
Yang <ferlik1988@gmail.com>
2025-11-14 23:01:57 +0200

    Add User entity and update database schema

--- WebMVC/Data/ApplicationDbContext.cs
+++ WebMVC/Data/ApplicationDbContext.cs
@@ -8,10 +8,14 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+        
     }
+    public DbSet<User> Users { get; set; }
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<User>().HasIndex(u => u.Email).IsUnique();
+        modelBuilder.Entity<User>().HasIndex(u => u.Username).IsUnique();
         base.OnModelCreating(modelBuilder);
     }
 }
--- WebMVC/obj/Debug/net9.0/WebMVC.AssemblyInfo.cs
+++ WebMVC/obj/Debug/net9.0/WebMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fd344492d42f2b7309894cfacd7716d8944c221")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59b51e581fb6a83e9a1b7d1518b1e47ec0e3d9d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
